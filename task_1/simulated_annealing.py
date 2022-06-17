3542c6f4fcd4b6069116bfbe4d8b20e10231790c
Nazrawi Demeke <dnazrawi@gmail.com>
2022-06-17 21:35:44 +0300

    initial commit

---
+++ task_1/simulated_annealing.py
@@ -0,0 +1,16 @@
+class SimulatedAnnealing:
+    def __init__(self, temperature=2):
+        self.temperature = temperature
+        self.optimum = float('-inf')
+        ...
+
+    def getRandomSolution(self):
+        ...
+
+    def getProbability(self, ):
+        ...
+
+    def solve(self):
+        ...
+
+
