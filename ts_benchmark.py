88b1a0c5d2f45f8187f88e7019cfbd42d832af25
Nazrawi Demeke <dnazrawi@gmail.com>
2022-06-22 18:14:29 +0300

    setup setup ts benchmark

---
+++ ts_benchmark.py
@@ -0,0 +1,29 @@
+import argparse
+from travelling_salesman.simulated_annealing import *
+from travelling_salesman.genetic import *
+from travelling_salesman.hill_climbing import *
+from travelling_salesman.graph import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--algorithm', type=str, required=True)
+    parser.add_argument('--file', type=str, required=True)
+    args = parser.parse_args()
+
+    graph = Graph(args.file)
+    print(f'Number of cities: {len(graph.nodes)}')
+
+    if args.algorithm == 'ga':
+        print('Solving with Genetic Algorithm...')
+        GeneticAlgo().solve(graph)
+
+    elif args.algorithm == 'hc':
+        print('Solving with Hill Climbing Algorithm...')
+        HillClimbing().solve(graph)
+
+    elif args.algorithm == 'sa':
+        print('Solving with Simulated Annealing Algorithm...')
+        SimulatedAnnealing().solve(graph)
+
+    else:
+        raise Exception(f'Invalid Algorithm: {args.algorithm}')
